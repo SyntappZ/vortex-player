73a0ba92c5b0fb41687b1b55d47a75505ecb870d
syntappz <doddsanater87@gmail.com>
2021-01-22 16:10:45 +0000

    added main packages

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vortex-player
+
+## this is vortex player upgrade as the first one had some bugs
+
